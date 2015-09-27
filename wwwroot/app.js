77229c5367b7e48768bb0a0f77685086e42b82e0
noopurtanna <noopur.tanna@gmail.com>
2015-09-27 05:17:58 -0400

    directions api

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -15,9 +15,29 @@ var hackGT = angular.module('hackGT', ['ngRoute']).config(function ($routeProvid
     redirectTo: "Views/home.html"
   });
 });
-hackGT.controller('homeCtrl', function homeCtrl ($scope) {
-  $scope.test="hello";
-});
-hackGT.controller('accountCtrl', function accountCtrl ($scope) {
-  $scope.test2="bye";
+hackGT.controller('homeCtrl', ['$scope', 'dirFactory', function ($scope, dirFactory) {
+  if (dirFactory.service.directions != null) {
+    $scope.directions = dirFactory.service.directions;
+  } else {
+    $scope.directions = dirFactory.service.getData;
+  }
+}]);
+// hackGT.controller('accountCtrl', function accountCtrl ($scope) {
+
+// });
+hackGT.factory('dirFactory', function ($http) {
+  var service = {
+    getData: getData,
+    directions: null
+  };
+
+  return service;
+
+  function getData() {
+    $http.get('https://maps.googleapis.com/maps/api/js?key=AIzaSyDAeGEUraKxK8Le140slXlIiC_sogId89M&callback=initMap')
+    .success(function (response) {
+      service.directions = response;
+      return response;
+    });
+  }
 });
